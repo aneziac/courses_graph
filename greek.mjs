2f20ddd291a5a4e06958633e59934e0efd4a1461
Ashwin <ashwinrajan@ucsb.edu>
2023-03-02 22:11:07 -0800

    updating greek.mjs so it actually works

--- greek.mjs
+++ greek.mjs
@@ -5,9 +5,18 @@ const graph = new Graph();
 
 var json = JSON.parse(fs.readFileSync('/Users/ashwin/Documents/courses_graph/greek.json', 'utf8'));
 
-for (var key in json.jsonData) {
-    graph.addNode(key);
-    for (var prereqs in json.jsonData[key][5]) {
-        graph.addEdge(key, prereqs);
+for (var key in json) {
+    graph.addNode(json[key].number);
+    for (var prereq in json[key].prereqs) {
+        for (var i = 0; i < json[key].prereqs.length; i++) {
+            for (var j = 0; j < json[key].prereqs[i].length; j++) {
+                if (json[key].prereqs[i][j] != 0) {
+                    var prereqName = json[key].prereqs[i][j];
+                    var prereqNum = prereqName.substring(6);
+                    graph.addEdge(json[key].number, prereqNum);
+
+                }
+            }
+        }
     }
 }
