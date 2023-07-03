edf089ed5cc7355ce30de60022c83cc71b0d18ab
Nate Annau <nateannau@gmail.com>
2023-07-02 18:05:41 -0700

    Remove old UI stuff

---
+++ src/components/SearchItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="searchItem">
+        <slot>Placeholder</slot>
+    </div>
+</template>
+
+<style>
+.searchItem {
+    background-color: aqua;
+    margin-top: 20px;
+    width: 60vw;
+    height: 7vh;
+    margin-left: 13vw;
+    font-size: 20px;
+}
+</style>
