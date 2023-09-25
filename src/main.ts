bc9fb3dc73e9315279732d3272c9c957d7c85d4b
Nate Annau <nateannau@gmail.com>
2023-09-25 14:47:54 -0700

    Hopefully actions are passing now

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - can't figure out typeerror, come back to this later
 import { createApp } from 'vue/dist/vue.esm-bundler';
 import { router } from './router'
